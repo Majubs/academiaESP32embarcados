d014571b3880f7e402d0c8b802ec33a5378780c7
majubs-itech <maria.berriel@itech.org.br>
2024-09-05 18:00:50 -0300

    Aula Hello World

--- blink/main/blink.c
+++ blink/main/blink.c
@@ -5,29 +5,30 @@ uma mensagem na serial com o status do LED.
 Autor: Fábio Souza
 ****************************************************************/
 
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
 // Função principal do programa
-void app_main(void) {
-  // Configuração do pino GPIO para o LED (número do pino pode variar)
-  gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
-  // jaksgsgduhid
+void app_main(void)
+{
+    // Configuração do pino GPIO para o LED (número do pino pode variar)
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
-  printf("Hello World! \n"); // Mensagem de inicialização
-  int a[10];
-  printf("%d", a[1]);
+    printf("Hello World! \n"); // Mensagem de inicialização
+    int a[10];
+    printf("%d", a[1]);
 
-  while (1) // Loop infinito
-  {
-    gpio_set_level(GPIO_NUM_2, 1);         // Liga o LED
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Aguarda 1 segundo
-    printf("LED - ON \n");                 // Mensagem de LED ligado
+    while (1) // Loop infinito
+    {
+        gpio_set_level(GPIO_NUM_2, 1);         // Liga o LED
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Aguarda 1 segundo
+        printf("LED - ON \n");                 // Mensagem de LED ligado
 
-    gpio_set_level(GPIO_NUM_2, 0);         // Desliga o LED
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Aguarda 1 segundo
-    printf("LED - OFF \n");                // Mensagem de LED desligado
-  }
+        gpio_set_level(GPIO_NUM_2, 0);         // Desliga o LED
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Aguarda 1 segundo
+        printf("LED - OFF \n");                // Mensagem de LED desligado
+    }
 }
