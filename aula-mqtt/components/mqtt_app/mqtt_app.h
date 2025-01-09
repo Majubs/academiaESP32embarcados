c226cb553b9193abc75b4b74d94a04b7368b633c
Majubs <mjuliabs@gmail.com>
2025-01-08 22:10:23 -0300

    Aula - MQTT pub/sub

---
+++ aula-mqtt/components/mqtt_app/mqtt_app.h
@@ -0,0 +1,13 @@
+#ifndef MQTT_APP_H
+#define MQTT_APP_H
+
+#include "esp_log.h"
+
+void mqtt_app_start(void);
+
+void mqtt_app_subscribe(char *topic, int qos);
+void mqtt_app_unsubscribe(char *topic);
+
+void mqtt_app_publish(char *topic, char *payload, int qos, int retain);
+
+#endif // !MQTT_APP_H
--- aula-mqtt/main/aula-mqtt.c
+++ aula-mqtt/main/aula-mqtt.c
@@ -8,49 +8,11 @@
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
-#include "mqtt_client.h"
 
 #include "wifi.h"
+#include "mqtt_app.h"
 
-static const char *TAG = "MQTT";
-
-static esp_mqtt_client_handle_t client; // handler for mqtt client
-
-static void mqtt_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    ESP_LOGI(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", event_base, event_id);
-    esp_mqtt_event_handle_t event = event_data;
-
-    switch ((esp_mqtt_event_id_t)event_id)
-    {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED");
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED");
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED");
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("topic: %.*s\n", event->topic_len, event->topic);
-        printf("message: %.*s\n", event->data_len, event->data);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGE(TAG, "ERROR %s", strerror(event->error_handle->esp_transport_sock_errno));
-        break;
-    default:
-        ESP_LOGI(TAG, "Outro Evento id:%d", event->event_id);
-        break;
-    }
-}
+static const char *TAG = "MAIN";
 
 void app_main(void)
 {
@@ -67,13 +29,13 @@ void app_main(void)
     ESP_ERROR_CHECK(wifi_init_sta());
     ESP_LOGI(TAG, "WiFi connected");
 
-    esp_mqtt_client_config_t esp_mqtt_client_config = {
-        .broker.address.uri = "mqtt://mqtt.eclipseprojects.io",
-        .broker.address.port = 1883,
-    };
+    mqtt_app_start();
+    ESP_LOGI(TAG, "MQTT connected");
 
-    ESP_LOGI(TAG, "Starting MQTT client");
-    client = esp_mqtt_client_init(&esp_mqtt_client_config);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-    esp_mqtt_client_start(client);
+    mqtt_app_subscribe("esp32c5/var", 0);
+
+    mqtt_app_publish("esp32c5/temp", "69", 0, 0);
+    ESP_LOGI(TAG, "Message published");
+
+    mqtt_app_unsubscribe("esp32c5/var");
 }
